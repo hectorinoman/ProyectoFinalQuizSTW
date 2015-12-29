6e720d5707ef81e956e921fd9d7dd20e2ee95593
hectorinoman <hectorinoman@gmail.com>
2015-12-29 22:00:54 +0000

    models, quiz y controller

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,42 +1,58 @@
-var Quiz = require('../models/quiz_model');
 
-var quiz = new Quiz();
-var current = quiz.randomQuestion();
+var models = require('../models/models.js');
 
-exports.question = function(req,res) {
-  current = quiz.randomQuestion();
-  res.render('quizes/question', {pregunta: current.pregunta});
+exports.question = function(req, res){
+	models.Quiz.findAll().success(function(quiz){
+		res.render('quizes/question', {pregunta: quiz[0].pregunta});
+	});
 };
 
+exports.answer = function(req, res){
+	models.Quiz.findAll().success(function(quiz){
+		if(req.query.respuesta === quiz[0].respuesta) {
+			res.render('quizes/answer', { respuesta: 'Correcto' });
+		} else {
+			res.render('quizes/answer', { respuesta: 'Incorrecto' });
+		}
+	});
+};
+
+var Quiz = require('../models/quiz_model');
+var quiz = new Quiz();
+var current = quiz.randomQuestion();
+exports.index = function(req, res, next) {
+	res.render('index', { title: 'Quiz' });
+};
+exports.question = function(req,res) {
+	current = quiz.randomQuestion();
+	res.render('quizes/question', {pregunta: current.pregunta });
+};
 exports.answer = function(req, res) {
-  var c = 'Incorrecto';
-  if (current.respuesta(req.query.respuesta)) { c = 'Correcto'; }
-  res.render('quizes/answer', {respuesta: c})
+	var c = 'Incorrecto';
+	if (current.respuesta(req.query.respuesta)) { c = 'Correcto'; }
+	res.render('quizes/answer', {respuesta: c});
 };
 
 exports.questions = function(req,res) {
-  var nPreg = quiz.numQuestions();
-  var array = new Array(nPreg);
-
-  for(var i=0; i<nPreg; i++) {
-    array[i] = (quiz.getQ(i));
-  }
-
-  res.render('quizes/questions', {prg: array})
+	var nPreg = quiz.numQuestions();
+	var array = new Array(nPreg);
+	for(var i=0; i<nPreg; i++) {
+		array[i] = quiz.getQ(i);
+	}
+	res.render('quizes/questions', {prg: array});
 };
 
 exports.specificQuestion = function(req, res) {
-  var id = req.params.id;
-  var nPreg = quiz.numQuestions();
-
-  if(id < 1 || id > nPreg){
-    res.render('quizes/SpecificQuestion', {prg: "No existe esa pregunta."})
-  }
-  else if(isNaN(id) === true) {
-    res.render('quizes/SpecificQuestion', {prg: "Error en la URL."})
-  }
-  else {
-    current = quiz.q[id-1];
-    res.render('quizes/question', {pregunta: current.pregunta});
-  }
+	var id = req.params.id;
+	var nPreg = quiz.numQuestions();
+	if(id < 1 || id > nPreg){
+		res.render('quizes/SpecificQuestion', {prg: "No existe esa pregunta."});
+	}
+	else if(isNaN(id) === true) {
+		res.render('quizes/SpecificQuestion', {prg: "Error en la URL."});
+	}
+	else {
+		current = quiz.q[id-1];
+		res.render('quizes/question', {pregunta: current.pregunta});
+	}
 };
---
+++ models/models.js
@@ -0,0 +1,28 @@
+
+var path = require('path');
+
+//Cargar modelo ORM
+var Sequelize = require('sequelize');
+
+//Usar BBDD SQLite
+var sequelize = new Sequelize(null, null, null,
+		{dialect: "sqlite", storage: "quiz.sqlite"}
+	);
+
+//Importar la definición de la tabla Quiz en quiz.js
+var Quiz = sequelize.import(path.join(__dirname, 'quiz'));
+
+exports.Quiz = Quiz; //exportar definición de la tabla Quiz (models/quiz.js)
+
+//sequelize.sync() crea e inicia tabla de preguntas en la base de datos
+sequelize.sync().success(function() {
+	Quiz.count().success(function (count) {
+		if(count === 0) {
+			Quiz.create({pregunta: 'Capital de Italia',
+						respuesta: 'Roma'
+			})
+			.success(function(){console.log('Base de datos inicializada');});
+		}
+	});
+
+});
