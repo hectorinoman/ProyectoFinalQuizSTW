e018d28bda1c1faa3e4bfc04665e29b03910d15d
hectorinoman <hectorinoman@gmail.com>
2016-01-04 17:40:26 +0000

    empezando a borrar esperando por editar

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -64,3 +64,34 @@ exports.create = function (req, res) {
 		);
 };
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+exports.destroy = function(req, res){
+	req.quiz.destroy().then ( function(){
+	res.redirect('/quizes');
+}).catch(function(error){next (error)});
+};
--- routes/index.js
+++ routes/index.js
@@ -23,4 +23,7 @@ router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
 router.get('/quizes/:new', quizController.new);
 router.post('/quizes/:create', quizController.create);
 
+
+router.delete('/quizes/:quizId(\\d+)', 		quizController.destroy);
+
 module.exports = router;
