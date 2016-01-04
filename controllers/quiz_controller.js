bf7c7e7b0809a31e50a76ec84d6f7c403a315431
OscarTrujillo <oscartrujillo94@gmail.com>
2016-01-04 15:48:57 +0000

    QUIZ 10 FUNCIONANDO

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -32,54 +32,9 @@ exports.answer = function(req, res){
 };
 
 exports.index = function(req, res){
-	models.Quiz.findAll().then(function(quizes) {
-		res.render('quizes/index.ejs', {quizes: quizes});
-	})
+	models.Quiz.findAll().then(
+		function(quizes) {
+			res.render('quizes/index.ejs', {quizes: quizes});
+	}).catch(function(error) {next(error);})
 };
 
-
-
-
-/*
-
-var Quiz = require('../models/quiz_model');
-var quiz = new Quiz();
-var current = quiz.randomQuestion();
-exports.index = function(req, res, next) {
-	res.render('index', { title: 'Quiz' });
-};
-exports.question = function(req,res) {
-	current = quiz.randomQuestion();
-	res.render('quizes/question', {pregunta: current.pregunta });
-};
-exports.answer = function(req, res) {
-	var c = 'Incorrecto';
-	if (current.respuesta(req.query.respuesta)) { c = 'Correcto'; }
-	res.render('quizes/answer', {respuesta: c});
-};
-
-exports.questions = function(req,res) {
-	var nPreg = quiz.numQuestions();
-	var array = new Array(nPreg);
-	for(var i=0; i<nPreg; i++) {
-		array[i] = quiz.getQ(i);
-	}
-	res.render('quizes/questions', {prg: array});
-};
-
-exports.specificQuestion = function(req, res) {
-	var id = req.params.id;
-	var nPreg = quiz.numQuestions();
-	if(id < 1 || id > nPreg){
-		res.render('quizes/SpecificQuestion', {prg: "No existe esa pregunta."});
-	}
-	else if(isNaN(id) === true) {
-		res.render('quizes/SpecificQuestion', {prg: "Error en la URL."});
-	}
-	else {
-		current = quiz.q[id-1];
-		res.render('quizes/question', {pregunta: current.pregunta});
-	}
-};
-
-*/
